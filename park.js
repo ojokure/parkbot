d066ea3481b45ba98ae3bd76d342cab83fae90d6
oladimeji-ojo <ojokuredim@gmail.com>
2020-05-28 19:51:57 +0100

    tweak

--- park.js
+++ park.js
@@ -1,34 +1,34 @@
-// async function sendWhatsApp(to, body, media) {
-//   const auth = "twilio_clientid:twilio_api";
-//   const sendURL =
-//     "https://api.twilio.com/2010-04-01/Accounts/{account_id}/Messages.json";
-//   const res = await fetch(sendURL, {
-//     headers: {
-//       Authorization: "Basic " + Buffer.from(auth).toString("base64"),
-//     },
-//     method: "POST",
-//     body: objToFORM(
-//       JSONRemoveUndefined({
-//         To: "whatsapp:+972" + to.replace(/-/g, "").replace(/^0/, ""),
-//         From: "whatsapp:+18454069614",
-//         Body: body,
-//         MediaUrl: media,
-//       })
-//     ),
-//   });
-// }
+async function sendWhatsApp(to, body, media) {
+  const auth = "twilio_clientid:twilio_api";
+  const sendURL =
+    "https://api.twilio.com/2010-04-01/Accounts/{account_id}/Messages.json";
+  const res = await fetch(sendURL, {
+    headers: {
+      Authorization: "Basic " + Buffer.from(auth).toString("base64"),
+    },
+    method: "POST",
+    body: objToFORM(
+      JSONRemoveUndefined({
+        To: "whatsapp:+972" + to.replace(/-/g, "").replace(/^0/, ""),
+        From: "whatsapp:+18454069614",
+        Body: body,
+        MediaUrl: media,
+      })
+    ),
+  });
+}
 
-// function objToFORM(obj) {
-//     const params = new URLSearchParams()
-//     for (var a in obj) {
-//       params.append(a, obj[a])
-//     }
-//     return params
-//   }
+function objToFORM(obj) {
+  const params = new URLSearchParams();
+  for (var a in obj) {
+    params.append(a, obj[a]);
+  }
+  return params;
+}
 
-//   function JSONRemoveUndefined(obj) {
-//     return JSON.parse(JSON.stringify(obj))
-//   }
+function JSONRemoveUndefined(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
